5adfbee3ef3368df02860c37db9dbe22ee12c8ff
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2014-06-29 21:41:00 -0700

    Missing fixes for mysql module

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,16 +1,16 @@
 class mysql {
    case $operatingsystem {
       redhat, centos: {
-         $servicename = 'mysqld',
-         $pkgname = 'mysql-server',
-         $config = 'my.conf.el',
-         $os_config = 'my.conf',
+         $servicename = 'mysqld'
+         $pkgname = 'mysql-server'
+         $config = 'my.conf.el'
+         $os_config = 'my.conf'
       }
       debian, ubuntu: {
-         $servicename = 'mysql',
-         $pkgname = 'mysql-server-5.5',
-         $config = 'my.conf.debian',
-         $os_config = 'mysql/my.conf',
+         $servicename = 'mysql'
+         $pkgname = 'mysql-server-5.5'
+         $config = 'my.conf.debian'
+         $os_config = 'mysql/my.conf'
       }
       default: { fail("Unrecognized operating system.") }
    }
