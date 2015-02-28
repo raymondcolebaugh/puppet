3af07d9b941f1af4a1bac5d001ef6d58f8e7b858
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2015-02-28 14:34:25 -0800

    Updated rvm module to use variable for ruby install version

--- modules/rvm/manifests/init.pp
+++ modules/rvm/manifests/init.pp
@@ -1,5 +1,5 @@
 class rvm {
-  $ruby_version = '2.0.0'
+  $ruby_version = '2.0.0-mri'
 
   exec {'rvm-key':
     command => 'curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -',
@@ -15,9 +15,9 @@ class rvm {
     before => Group['rvm']
   }
 
-  exec {'use-ruby-2.0.0-mri':
+  exec {"use-ruby-${ruby_version}":
     # Must be executed in subshell to load rvm as a function
-    command => 'su -c "source /etc/profile.d/rvm.sh && rvm use --default 2.0.0-mri"',
+    command => "su -c 'source /etc/profile.d/rvm.sh && rvm use --default ${ruby_version}'",
     require => Exec['rvm']
   }
 
