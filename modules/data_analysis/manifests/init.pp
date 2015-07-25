b4270e24e7114584707ea8fd0ad32511c8cb52ee
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2015-07-25 00:06:25 -0700

    Improved dependency installation for data_analysis module

--- modules/data_analysis/manifests/init.pp
+++ modules/data_analysis/manifests/init.pp
@@ -1,3 +1,4 @@
+# Install various packages and python modules for data analysis
 class data_analysis {
   $deps = ['gfortran', 'liblapack-dev']
   package {$deps:
@@ -7,15 +8,18 @@ class data_analysis {
   package {[
     'ipython3-notebook',
     'scilab-cli',
+    'libfreetype6-dev',
     'weka']:
       ensure => latest,
   }
 
   package {[
     'numpy',
-    'scipy']:
-      provider => 'pip3',
+    'scipy',
+    'pandas',
+    'matplotlib']:
       ensure   => latest,
+      provider => 'pip3',
       require  => Package[$deps],
   }
 }
