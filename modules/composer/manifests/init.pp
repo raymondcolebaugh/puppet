2c4bd05e3ceb8c9555bb0d2be114684e571032ed
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2016-08-03 23:16:49 -0700

    Fixed composer module with COMPOSER_HOME

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -1,6 +1,8 @@
-class composer {
+class composer(
+  $composer_home = '/opt/composer',
+){
   exec {'composer':
-    command => 'curl -sS https://getcomposer.org/installer | php',
+    command => "curl -sS https://getcomposer.org/installer | COMPOSER_HOME=${composer_home} php",
     unless  => 'which composer'
   }
 
