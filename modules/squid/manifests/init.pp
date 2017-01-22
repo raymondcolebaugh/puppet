41fbdb2b28dc15c7aa15edbedd248cdac7714aa0
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2017-01-21 20:09:57 -0800

    Parameterized squid module

--- modules/squid/manifests/init.pp
+++ modules/squid/manifests/init.pp
@@ -1,28 +1,74 @@
-class squid {
+# Install Squid caching proxy
+class squid(
+  $http_allow = 'localhost',
+  $localnet = false,
+  $transparent = false,
+  $allow_manager = 'localhost',
+  $cache_mem = 128,
+  $memory_replacement = 'lru',
+  $cache_replacement = 'lru',
+  $fs_type = 'ufs',
+  $cache_dir = '/var/spool/squid',
+  $cache_size = 100,
+  $cache_l1 = 16,
+  $cache_l2 = 256,
+  $peer_hostname = false,
+  $peer_type = 'parent',
+  $peer_proxy_port = 3128,
+  $peer_icp_port = 3130,
+  $peer_options = 'proxy-only',
+) {
   case $::operatingsystem {
-      debian, ubuntu: {
-         $servicename = 'squid'
+    debian, ubuntu: {
+      if $::distcodename == 'wheezy' or
+        $::distcodename == 'precise' {
+        $servicename = 'squid'
+      } else {
+        $servicename = 'squid3'
       }
-      default: { fail('Unrecognized operating system.') }
-   }
+    }
+    default: { fail('Unrecognized operating system.') }
+  }
 
-   package {'squid':
-      name   => $servicename,
-      ensure => 'latest',
-      before => Service['squid']
-   }
+  package {'squid':
+    ensure => 'latest',
+    name   => $servicename,
+    before => Service['squid']
+  }
 
-   service {'squid':
-      name       => $servicename,
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      require    => Package['squid'],
-   }
+  user {'squid':
+    ensure => present,
+    before => File['/var/spool/squid'],
+  }
 
-   file {'squid.conf':
-      path   => '/etc/squid/squid.conf',
-      mode   => '0640',
-      notify => Service['squid']
-   }
+  file {'/var/spool/squid':
+    ensure => directory,
+    mode   => '0770',
+    owner  => 'squid',
+    group  => 'squid',
+    before => Exec['squid-zero-cache'],
+  }
+
+  exec {'squid-zero-cache':
+    command     => "${servicename} -z ${cache_dir}",
+    refreshonly => true,
+    require     => Package['squid'],
+  }
+
+  service {'squid':
+    ensure     => running,
+    name       => $servicename,
+    enable     => true,
+    hasrestart => true,
+    require    => Package['squid'],
+  }
+
+  file {'squid.conf':
+    path    => "/etc/${servicename}/squid.conf",
+    mode    => '0640',
+    notify  => Service['squid'],
+    content => template("squid/${servicename}.conf.erb"),
+    require => Package['squid'],
+    before  => Exec['squid-zero-cache'],
+  }
 }
